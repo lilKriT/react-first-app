fa4f03137f2ca61a4790976efc6e3ad552c822a2
Piotr Filipiak <kiddhkane@gmail.com>
2022-07-18 22:19:58 +0200

    Made a book list.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Greeting() {
-  return <h4>This is my first React App</h4>;
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+function BookList() {
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
 
-ReactDOM.render(<Greeting />, document.getElementById("root"));
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => {
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL604_SR604,400_.jpg"
+      alt=""
+    ></img>
+  );
+};
+
+const Title = () => {
+  return (
+    <h1>
+      Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones
+    </h1>
+  );
+};
+
+const Author = () => {
+  return <h4>James Clear</h4>;
+};
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
