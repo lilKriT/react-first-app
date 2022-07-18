ad95a87ea4bcb2d218d0bb670c09c3fe4f219a9e
Piotr Filipiak <kiddhkane@gmail.com>
2022-07-18 18:09:36 +0200

    Installed React. Removed boilerplate. Created first component.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Greeting() {
+  return <h4>This is my first React App</h4>;
+}
+
+ReactDOM.render(<Greeting />, document.getElementById("root"));
