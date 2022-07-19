80a90f4d245caa5711f679f2bce477d127860a06
Piotr Filipiak <kiddhkane@gmail.com>
2022-07-19 20:20:56 +0200

    Added books as a list.

--- src/index.js
+++ src/index.js
@@ -7,28 +7,33 @@ import "./index.css";
 // vars
 const books = [
   {
+    id: 1,
     img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL604_SR604,400_.jpg",
     title:
       "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
     author: "James Clear",
   },
   {
+    id: 2,
     img: "https://images-na.ssl-images-amazon.com/images/I/81l17H47TJL._AC_UL604_SR604,400_.jpg",
     title: "It Starts with Us: A Novel",
     author: "Colleen Hoover",
   },
 ];
 
-const lorem = ["lorem", "ipsum", "dolor"];
-const newLorem = lorem.map((word) => {
-  return <h1>{word}</h1>;
-});
-
 function BookList() {
-  return <section className="booklist">{newLorem}</section>;
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        // const { img, title, author } = book;
+        return <Book key={book.id} book={book} />;
+      })}
+    </section>
+  );
 }
 
-const Book = ({ img, title, author }) => {
+const Book = (props) => {
+  const { img, title, author } = props.book;
   return (
     <article className="book">
       <img src={img} alt=""></img>
